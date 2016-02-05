b82dc3473e631f696ae63a361e1cda082fd7de0c
Sean McMains <sean@mcmains.net>
2016-02-05 10:26:48 -0600

    Basic IronCourt app set up.

---
+++ src/IronCourt/IronCourt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IronCourt
+//
+//  Created by Sean Mc Mains on 2/5/16.
+//  Copyright © 2016 Sean Mc Mains. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
